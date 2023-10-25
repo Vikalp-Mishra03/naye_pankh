a1e075d988440f1295a414f6c360e0b323297253
Vikalp Mishra <mishravikalp896@gmail.com>
2023-10-25 16:17:55 +0530

    certificate page complete

--- src/component/Certificates.jsx
+++ src/component/Certificates.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import certificate1 from '../assets/9.avif' 
+import certificate2 from '../assets/10.avif' 
+import certificate3 from '../assets/11.avif' 
 
 export default function Certificates() {
   return (
-    <div>
-      certificates
+    <div className='certificates'>
+      <h1>Our Certificates</h1>
+      <div className="certificate">
+        <img src={certificate1} alt="" />
+        <img src={certificate2} alt="" />
+        <img src={certificate3} alt="" />
+      </div>
     </div>
   )
 }
