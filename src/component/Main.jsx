082b2cd44e9bcec696d5987ecdf3b5c9f7ceb794
Vikalp Mishra <mishravikalp896@gmail.com>
2023-10-25 11:49:22 +0530

    main section complete

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -14,8 +14,8 @@ export default function Main() {
                     <p>"NayePankh Foundation" is a non governmental organisation with a strong desire to help the society and make it a better place for all, by doing everything in our power and to make our vision successful we would require your vital support. Service to mankind is the service to god. Let’s revolutionise the society together!.</p>
 
                     <div className="button">
-                        <button>LEARN MORE</button>
-                        <button>OUR CERTIFICATES</button>
+                        <button className='button1'>LEARN MORE</button>
+                        <button className='button2'>OUR CERTIFICATES</button>
                     </div>
                 </div>
             </div>
