ba20c8a2f06ea522cccd50f7826acc7a70721865
Vikalp Mishra <mishravikalp896@gmail.com>
2023-11-20 13:44:02 +0530

    adding feedback

--- src/component/Certificates.jsx
+++ src/component/Certificates.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import certificate1 from '../assets/9.avif' 
-import certificate2 from '../assets/10.avif' 
-import certificate3 from '../assets/11.avif' 
-
+import certificate1 from '../assets/9.avif'
+import certificate2 from '../assets/10.avif'
+import certificate3 from '../assets/11.avif'
+import Feedback from './Feedback'
 export default function Certificates() {
   return (
-    <div className='certificates'>
-      <h1>Our Certificates</h1>
-      <div className="certificate">
-        <img src={certificate1} alt="" />
-        <img src={certificate2} alt="" />
-        <img src={certificate3} alt="" />
+    <>
+      <div className='certificates'>
+        <h1>Our Certificates</h1>
+        <div className="certificate">
+          <img src={certificate1} alt="" />
+          <img src={certificate2} alt="" />
+          <img src={certificate3} alt="" />
+        </div>
       </div>
-    </div>
+      <Feedback />
+    </>
   )
 }
