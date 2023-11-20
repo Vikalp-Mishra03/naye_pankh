498348496eb4015c24d2fd4e87268adb94dcade0
Vikalp Mishra <mishravikalp896@gmail.com>
2023-11-20 13:44:23 +0530

    adding feedback

--- src/component/Newspaper.jsx
+++ src/component/Newspaper.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import newspaper from '../assets/12.avif'
-
+import Feedback from './Feedback'
 export default function Newspaper() {
   return (
-    <div className='newspaper'>
-      <img src={newspaper} alt="" />
-    </div>
+    <>
+      <div className='newspaper'>
+        <img src={newspaper} alt="" />
+      </div>
+      <Feedback />
+    </>
   )
 }
